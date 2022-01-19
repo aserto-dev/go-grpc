9ff17b6fc2097d970292d64f6f5ec3dbcc1ff3f1
viovanov <vlad@aserto.com>
2022-01-19 17:11:17 +0200

    Standardize on org vs owner and add getrepo API

--- aserto/tenant/policy_builder/v1/policy_builder.pb.gw.go
+++ aserto/tenant/policy_builder/v1/policy_builder.pb.gw.go
@@ -31,9 +31,20 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+var (
+	filter_PolicyBuilder_ListPolicyBuilders_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_PolicyBuilder_ListPolicyBuilders_0(ctx context.Context, marshaler runtime.Marshaler, client PolicyBuilderClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListPolicyBuildersRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PolicyBuilder_ListPolicyBuilders_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.ListPolicyBuilders(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -43,6 +54,13 @@ func request_PolicyBuilder_ListPolicyBuilders_0(ctx context.Context, marshaler r
 func local_request_PolicyBuilder_ListPolicyBuilders_0(ctx context.Context, marshaler runtime.Marshaler, server PolicyBuilderServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListPolicyBuildersRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PolicyBuilder_ListPolicyBuilders_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.ListPolicyBuilders(ctx, &protoReq)
 	return msg, metadata, err
