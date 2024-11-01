24d460ad01fa8bf73cd242fa3d8119a3b496a0c9
Ronen Hilewicz <ronen@aserto.com>
2024-11-01 08:33:10 -0400

    Move `cerr` from the archived aserto-dev/go-utils

--- go.mod
+++ go.mod
@@ -3,15 +3,24 @@ module github.com/aserto-dev/go-grpc
 go 1.21
 
 require (
+	github.com/aserto-dev/errors v0.0.11
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20240805194559-2c9e96a0b5d4
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240805194559-2c9e96a0b5d4
+	github.com/stretchr/testify v1.9.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20240812133136-8ffd90a71988
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240812133136-8ffd90a71988
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/cerr/errors_test.go
@@ -0,0 +1,75 @@
+package cerr_test
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/aserto-dev/go-grpc/pkg/cerr"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+)
+
+func TestDoubleCerr(t *testing.T) {
+	assert := require.New(t)
+
+	err := cerr.ErrAccountNotFound.Err(cerr.ErrRepoAlreadyConnected)
+
+	assert.Contains(err.Error(), "already been connected")
+	assert.Contains(err.Error(), "account not found")
+}
+
+func TestDoubleCerrWithMsg(t *testing.T) {
+	assert := require.New(t)
+
+	err := cerr.ErrAccountNotFound.Err(cerr.ErrRepoAlreadyConnected).Msg("failed to setup")
+
+	assert.Contains(err.Error(), "already been connected")
+	assert.Contains(err.Error(), "account not found")
+}
+
+func TestWithEmptyMsg(t *testing.T) {
+	assert := require.New(t)
+
+	err := cerr.ErrAccountNotFound.Msg("")
+
+	fields := err.Fields()
+	assert.Nil(fields["msg"])
+
+	err = cerr.ErrAccountNotFound.Msg("bla")
+
+	fields = err.Fields()
+	assert.NotNil(fields["msg"])
+}
+
+func TestError(t *testing.T) {
+	assert := require.New(t)
+
+	err := cerr.ErrAccountNotFound.Msg("bla").Err(errors.New("boom"))
+	err2 := cerr.ErrAccountNotFound.Msg("bla").Msg("ala")
+	err3 := cerr.ErrAccountNotFound.Err(errors.New("boom")).Msg("bla").Msg("ala")
+	err4 := cerr.ErrAccountNotFound.Err(errors.New("boom")).Err(errors.New("pow")).Msg("bla").Msg("ala")
+	err5 := cerr.ErrAccountNotFound.Err(errors.New("boom"))
+	err6 := cerr.ErrAccountNotFound.Err(errors.New("boom")).Err(errors.New("pow"))
+	err7 := cerr.ErrAccountNotFound.Msg("bla")
+
+	assert.Equal(err.Error(), "E10012 account not found: boom: bla")
+	assert.Equal(err2.Error(), "E10012 account not found: bla: ala")
+	assert.Equal(err3.Error(), "E10012 account not found: boom: bla: ala")
+	assert.Equal(err4.Error(), "E10012 account not found: boom: pow: bla: ala")
+	assert.Equal(err5.Error(), "E10012 account not found: boom")
+	assert.Equal(err6.Error(), "E10012 account not found: boom: pow")
+	assert.Equal(err7.Error(), "E10012 account not found: bla")
+}
+
+func TestWithGrpcStatusCode(t *testing.T) {
+	assert := require.New(t)
+	err := cerr.ErrAccountNotFound.WithGRPCStatus(codes.Canceled)
+	assert.Equal(err.StatusCode, codes.Canceled)
+}
+
+func TestWithHttpStatusCode(t *testing.T) {
+	assert := require.New(t)
+	err := cerr.ErrAccountNotFound.WithHTTPStatus(http.StatusAccepted)
+	assert.Equal(err.HTTPCode, http.StatusAccepted)
+}
